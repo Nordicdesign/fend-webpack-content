2938eed647d05462ad85b0412da024921da1c531
Alex <alex.lillo@gmail.com>
2021-07-13 13:36:42 +0100

    added html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const webpack = require('webpack')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
 const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
@@ -13,5 +14,11 @@ module.exports = {
       }
     ]
   },
-  plugins: [new ESLintPlugin()]
+  plugins: [
+    new ESLintPlugin(),
+    new HtmlWebPackPlugin({
+      template: './src/client/views/index.html',
+      filename: './index.html'
+    })
+  ]
 }
