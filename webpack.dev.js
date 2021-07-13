55fff367363c3ec66a47afffea089137d47838f0
Alex <alex.lillo@gmail.com>
2021-07-13 14:24:46 +0100

    added bundle-analyser

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,7 +3,7 @@ const webpack = require('webpack')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 const ESLintPlugin = require('eslint-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-// const WebpackDevServer = require('webpack-dev-server')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   mode: 'development',
@@ -27,6 +27,15 @@ module.exports = {
       template: './src/client/views/index.html',
       filename: './index.html'
     }),
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin({
+      // Simulate the removal of files
+      dry: true,
+      // Write Logs to Console
+      verbose: true,
+      // Automatically remove all unused webpack assets on rebuild
+      cleanStaleWebpackAssets: true,
+      protectWebpackAssets: false
+    }),
+    new BundleAnalyzerPlugin()
   ]
 }
