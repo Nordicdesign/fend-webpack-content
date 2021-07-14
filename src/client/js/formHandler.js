11a7f08e97a2b7b901f18670c0702f80ec98edf1
Alex <alex.lillo@gmail.com>
2021-07-14 17:01:00 +0100

    gets the posible origin of a name

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,17 +1,23 @@
 import { checkForName } from './nameChecker'
+import CountryQuery from 'country-query'
 
 export function handleSubmit(event) {
   event.preventDefault()
 
   // check what text was put into the form field
   let formText = document.getElementById('name').value
-  console.log(formText)
   checkForName(formText)
 
-  // console.log("::: Form Submitted :::")
-  fetch('http://localhost:8080/test')
+  const url = 'https://api.nationalize.io?name='
+  fetch(url + formText)
     .then(res => res.json())
     .then(function(res) {
-      document.getElementById('results').innerHTML = res.message
+      let possibleCountries = []
+      for (const country of res.country) {
+        let place = CountryQuery.find('cca2', country.country_id)
+        possibleCountries.push(place.name.common)
+      }
+      possibleCountries.join(', ')
+      document.getElementById('results').innerHTML = possibleCountries
     })
 }
